2c0fa86b928f232a516b7978d846fe8efa45259a
Brandon Bang <brandoncharlesbang@gmail.com>
2022-05-24 15:28:26 -0400

    Set background color to white

--- src/components/forms/Field.vue
+++ src/components/forms/Field.vue
@@ -2,7 +2,7 @@
   <label class="flex flex-col">
     <span>{{ label }}</span>
     <input
-        class="border-2 border-slate-800 px-3 py-1.5 text-base placeholder:text-slate-400 focus:outline-none focus:ring-4 focus:ring-slate-300"
+        class="border-2 border-slate-800 px-3 py-1.5 text-base bg-white placeholder:text-slate-400 focus:outline-none focus:ring-4 focus:ring-slate-300"
         :type="type"
         :placeholder="placeholder"
         :value="modelValue"
