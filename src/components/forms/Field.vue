c235661525edc6ea92ec6dfc7fafe465a3512671
Brandon Bang <brandoncharlesbang@gmail.com>
2022-05-24 20:38:51 -0400

    Add "required" attribute

--- src/components/forms/Field.vue
+++ src/components/forms/Field.vue
@@ -5,6 +5,7 @@
         class="border-2 border-slate-800 px-3 py-1.5 text-base bg-white placeholder:text-slate-400 focus:outline-none focus:ring-4 focus:ring-slate-300"
         :type="type"
         :placeholder="placeholder"
+        :required="required"
         :value="modelValue"
         @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
     />
@@ -16,6 +17,7 @@
     type?: string;
     label: string;
     placeholder?: string;
+    required?: boolean;
     modelValue?: any|null;
   }
 
