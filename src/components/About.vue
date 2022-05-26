7e37f38c48b176755ed5c853a1890d32b95083e2
Brandon Bang <brandoncharlesbang@gmail.com>
2022-05-26 13:42:50 -0400

    Fix capitalization

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,5 @@
 <template>
   <h2 class="text-2xl">About</h2>
-  <p class="mt-4">Calories.Watch is a Calorie tracking web app that can be used from any device, and works entirely offline.</p>
+  <p class="mt-4">Calories.watch is a calorie tracking web app that can be used from any device, and works entirely offline.</p>
   <p class="mt-4">Made by <a class="font-semibold underline decoration-dotted text-slate-500 decoration-2" href="https://brandonbang.com">Brandon Bang</a>.</p>
 </template>
