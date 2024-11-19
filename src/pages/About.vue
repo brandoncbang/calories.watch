845e0526e2d5c807f0c740a4a3686711f6822085
Brandon Bang <brandoncharlesbang@gmail.com>
2024-11-19 10:26:39 -0500

    Add client side routing

---
+++ src/pages/About.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h2 class="text-2xl">About</h2>
+  <p class="mt-4">Calories.watch is a calorie tracking web app that can be used from any device, and works entirely
+    offline.</p>
+  <p class="mt-4">Made by <a class="font-semibold underline decoration-dotted text-slate-500 decoration-2"
+      href="https://brandonbang.com">Brandon Bang</a>.</p>
+</template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, incidunt quo! Corrupti, tempora! Deserunt
+    reiciendis, eligendi ut non inventore officia aspernatur. Saepe sapiente inventore, perspiciatis ducimus laudantium
+    veniam vitae ea ab temporibus cupiditate consectetur ad provident quibusdam aliquam magni expedita iure voluptates,
+    adipisci quis architecto enim dicta iusto. Nobis corporis temporibus ratione tenetur vitae voluptas ducimus
+    praesentium voluptatum. Eveniet voluptatem voluptate natus deserunt voluptatibus nihil recusandae quo inventore
+    dolore. Laboriosam fugit rerum labore illo eius corrupti. Ipsa asperiores harum laborum voluptate quam dolores
+    adipisci nisi repellendus. Labore assumenda voluptatibus expedita sit iusto adipisci, magni commodi debitis
+    similique ipsa, eligendi ab nemo illum dolore voluptatem culpa sint illo ad fugiat error harum pariatur?
+    Exercitationem animi repellat nesciunt sint similique architecto nostrum? Consectetur sint dolorem quisquam dolore!
+    Quibusdam libero nisi harum, rerum ex nemo dicta? Totam rem qui culpa? Praesentium reprehenderit nihil numquam
+    molestiae, libero temporibus molestias reiciendis nostrum aperiam in magnam explicabo est optio repellat blanditiis
+    dolor voluptatem aspernatur illum nulla eius ullam officia. Numquam qui ab ea dolores quo laborum eaque recusandae
+    eligendi id cupiditate, minus, eum dolore quibusdam commodi. Labore, cumque. Minus perferendis vel voluptates ea
+    quisquam dolore sapiente inventore earum laborum impedit laudantium cum dolorem dolor velit aliquid, fugiat eius
+    ipsum reprehenderit obcaecati nemo ullam. Repellendus aut perferendis expedita alias, earum esse placeat? Earum ea
+    non ipsa sapiente expedita quia ipsam perspiciatis alias harum a magnam dolorem pariatur consequatur, quae
+    cupiditate odit. Inventore labore obcaecati fuga, recusandae quam eveniet atque vel veritatis soluta, suscipit iste
+    quo in voluptatibus dolorum! Recusandae aspernatur autem beatae? Vero perferendis eligendi velit, mollitia commodi
+    pariatur ipsum dolor a veritatis nostrum ab, iste, saepe eius modi! Fugiat quisquam delectus rem error reiciendis
+    minus quia sunt accusantium pariatur quod repellat ea libero amet odit, iure ipsam dolores repellendus magnam
+    commodi, harum corporis in autem saepe iste. Facere sunt nesciunt saepe deleniti ab fugit aut aliquid, laudantium
+    nam. Fugiat quibusdam esse veniam recusandae, rem cumque alias assumenda quas eos aliquam. Voluptatum aut nemo quos
+    odit minus sapiente nisi laboriosam, quas soluta quam quaerat placeat sed neque consectetur tempore tempora
+    doloremque fugiat voluptatem consequuntur corrupti amet? Quia tenetur quos iste doloremque minima culpa,
+    perspiciatis nemo placeat similique quasi sint recusandae modi consequatur, excepturi enim distinctio dignissimos
+    veritatis! Quidem ab quasi numquam debitis nihil corporis maiores ullam, delectus totam, incidunt doloremque
+    laudantium laboriosam, inventore dolore qui itaque. Ipsum atque quos, minima explicabo deleniti odio tempore. Cumque
+    odio sit, ea deleniti voluptatibus facilis. Molestiae quasi voluptatibus perspiciatis beatae, provident magnam omnis
+    nihil! Id nobis saepe, voluptas doloremque minus, temporibus ipsam in laborum quas iste eius dicta enim nulla
+    eveniet illum debitis officia suscipit nisi cupiditate. Eius voluptas ab fugiat, corrupti id alias in sit doloremque
+    rem error hic. Laboriosam similique praesentium eligendi quis, fugiat et tempore, dolore saepe, ducimus molestiae
+    alias consectetur? Quidem quae magni excepturi nihil numquam fugiat vero, maiores distinctio repellat expedita.
+    Temporibus, nam incidunt perferendis corporis odit sed, omnis, cum accusamus molestiae non placeat est beatae
+    officia sit facere recusandae in aperiam alias ducimus porro eius?</p>
+</template>
