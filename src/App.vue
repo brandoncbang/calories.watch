7e51a1ebb70d187a9ec3bdcaeb3be9e75c56458a
Brandon Bang <brandoncharlesbang@gmail.com>
2024-11-20 17:12:10 -0500

    Fix app layout

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <h1 class="text-4xl font-bold">Hello, World!</h1>
+  <div class="flex flex-col min-h-screen">
+    <main class="flex-1">
+      <div class="px-6">
+        <div class="mx-auto max-w-3xl py-8">
+          <RouterView />
+        </div>
+      </div>
+    </main>
 
-  <main>
-    <RouterView />
-  </main>
-
-  <footer>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </footer>
+    <footer class="sticky bottom-0 border-t border-slate-900 bg-white">
+      <div class="mx-auto max-w-3xl">
+        <div class="mx-auto max-w-3xl py-4">
+          <nav class="flex justify-between items-center space-x-2">
+            <RouterLink class="text-xl py-4" to="/">Home</RouterLink>
+            <RouterLink class="text-xl py-4" to="/about">About</RouterLink>
+          </nav>
+        </div>
+      </div>
+    </footer>
+  </div>
 </template>
