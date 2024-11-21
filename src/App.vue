089b4f56ba15c371abff2576be22c5e8cde4b553
Brandon Bang <brandoncharlesbang@gmail.com>
2024-11-21 14:27:13 -0500

    Use smaller menu item text on small screens

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,15 @@
         <div class="mx-auto max-w-3xl">
           <div class="mx-auto max-w-3xl py-4">
             <nav class="flex justify-evenly items-center space-x-2">
-              <RouterLink class="text-xl px-3 py-4" to="/">Home</RouterLink>
-              <RouterLink class="text-xl px-3 py-4" to="/settings"
-                >Settings</RouterLink
-              >
-              <RouterLink class="text-xl px-3 py-4" to="/about"
-                >About</RouterLink
-              >
+              <RouterLink class="text-lg px-3 py-4 sm:text-xl" to="/"
+                >Home
+              </RouterLink>
+              <RouterLink class="text-lg px-3 py-4 sm:text-xl" to="/settings"
+                >Settings
+              </RouterLink>
+              <RouterLink class="text-lg px-3 py-4 sm:text-xl" to="/about"
+                >About
+              </RouterLink>
             </nav>
           </div>
         </div>
